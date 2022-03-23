5b6439d240cc692efcf4ad89641756617848f54c
LaupWing <loc_strict@hotmail.com>
2022-03-23 19:18:58 +0100

    this works

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 from selenium import webdriver
 
 # print(webdriver)
-os.environ['PATH'] += r"C:/Users/pin-d/Desktop/drivers"
-driver = webdriver.Chrome()
+# os.environ['PATH'] += r"C:/Users/pin-d/Desktop/drivers/chromedriver"
+driver = webdriver.Chrome(executable_path=r"C:/Users/pin-d/Desktop/drivers/chromedriver.exe")
