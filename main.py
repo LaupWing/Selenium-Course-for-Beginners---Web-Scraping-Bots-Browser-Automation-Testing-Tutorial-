a11884d9e4579ff2e7ada158d79fa5fd4b8cc3a2
LaupWing <loc_strict@hotmail.com>
2022-04-11 11:48:03 +0200

    Getting element

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from selenium import webdriver
 # print(webdriver)
 # os.environ['PATH'] += r"C:/Users/pin-d/Desktop/drivers/chromedriver"
 driver = webdriver.Chrome(executable_path=r"C:/Users/pin-d/Desktop/drivers/chromedriver.exe")
+
+# Getting element
+driver.get("https://www.seleniumeasy.com/")
+driver.find_element_by_class_name('.btn.btn-success')
