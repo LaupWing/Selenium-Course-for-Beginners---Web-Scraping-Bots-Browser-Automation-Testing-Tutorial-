245014e51e404b5d2e6188cc70d050d212f2a21e
LaupWing <loc_strict@hotmail.com>
2022-04-11 12:22:30 +0200

    try except

--- main.py
+++ main.py
@@ -9,6 +9,12 @@ driver = webdriver.Chrome(executable_path=r"C:/Users/pin-d/Desktop/drivers/chrom
 driver.implicitly_wait(30) # Waits for 30 seconds but stops when it finds it
 # Getting element
 driver.get("https://jqueryui.com/progressbar/#download")
+
+try:
+   no_button = driver.find_element(By.CLASS_NAME, 'at-cm-no-button')
+   no_button.click()
+except:
+   print('No element found')
 button = driver.find_element(By.CSS_SELECTOR,'#downloadButton')
 print(button)
 button.click()
