9da894da9c272abee9d952f8b07437d8aff66256
LaupWing <loc_strict@hotmail.com>
2022-04-11 11:55:45 +0200

    implicityl wait

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium import webdriver
 # os.environ['PATH'] += r"C:/Users/pin-d/Desktop/drivers/chromedriver"
 driver = webdriver.Chrome(executable_path=r"C:/Users/pin-d/Desktop/drivers/chromedriver.exe")
 
+driver.implicitly_wait(30) # Waits for 30 seconds but stops when it finds it
 # Getting element
 driver.get("https://www.seleniumeasy.com/")
 button = driver.find_element_by_css_selector('.btn.btn-success')
